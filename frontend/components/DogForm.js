b0d23f3d1eccc644964463c1f166c80104b35825
BugzeeDev <irvingcsylva@gmail.com>
2025-02-15 18:59:21 -0800

    added sorting to the breed list drop down for ease of reading

--- frontend/components/DogForm.js
+++ frontend/components/DogForm.js
@@ -11,7 +11,7 @@ export default function DogForm({ dog, reset, getDogs }) {
   useEffect(() => {
     fetch('/api/dogs/breeds')
     .then(res => res.json())
-    .then(breeds => setBreeds(breeds))
+    .then(breeds => setBreeds(breeds.toSorted()))
     .catch(err => console.error(err))
   }, [])
   useEffect(() => {
